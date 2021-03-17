cfad4cad3ba7488e4385d50d0f3269673ce35e54
data19 <data19@DESKTOP-13EEMB9>
2021-03-17 11:46:38 +0900

    프로그래밍 1

--- sangwun/src/sangwun.java
+++ sangwun/src/sangwun.java
@@ -2,15 +2,18 @@
 public class sangwun {
 
 	public static void main(String[] args) {
+		int a;
+		float b;
+		
+		a = (int)123.45f;
+		b = 200;
+		
+		System.out.println("a의 값 |: "+ a);
+		System.out.println("b의 값 |: "+ b);
+		
+		System.out.println("편함");
+		
 	
-		
-		
-		
-		
-		
-		
-		
-		int a = 5;
 
 	}
 
