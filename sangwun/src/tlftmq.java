0c2d2e860513eb9835588d65985c7b10204bd0c5
data19 <data19@DESKTOP-13EEMB9>
2021-03-17 11:21:58 +0900

    ad

---
+++ sangwun/src/tlftmq.java
@@ -0,0 +1,26 @@
+class tlftmq {
+
+	public static void main(String[] args) {
+		int a,b,c,d;
+		
+		
+		a= 100+100;
+		b= a+100;
+		c= a+b-100;
+		d= a+b+c;
+		
+		System.out.println("a, b, c,dÀÇ°ª="+ a + " "+b+" "+c+" "+d);
+		
+		a = b = c = d = 100;
+		System.out.println("a:"+a);
+		System.out.println("b:"+b);
+		System.out.println("c:"+c);
+		System.out.println("d:"+d);
+		
+		
+		a=100;
+		a=a+200;
+		System.out.println("a:"+a);
+		
+	}
+} 
